fca059135f9e84d50b0b424626a854bd10556708
GimmeSpoon <skcjfdnd1996@naver.com>
2020-09-04 13:05:58 +0900

    Add files via upload

--- call.js
+++ call.js
@@ -1,14 +1,14 @@
 
 var request = require("request")
-
 var i = 0
+
 function doit () {
 	v = Math.random() * 100
 
 	request("https://api.thingspeak.com/update?api_key=JBFKFKUF2QCPSL3B&field1="+v, function(err,res, body) {console.log(body)})
 
 	console.log('i=${i}')
-	if(i++ > 10) return;
+	if(i++ > 10) return
 	setTimeout(doit, 20000);
 }
 
